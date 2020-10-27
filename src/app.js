1e4591305aa1d085dc5b6d2f473bfb8bf8aeb543
Brenox889 <brenomendes889@hotmail.com>
2020-10-27 19:57:34 -0300

    feat: repository routes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuid, validate: isUuid } = require('uuid');
 
 const app = express();
 
@@ -11,23 +11,73 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
-});
+  return response.send(repositories);}
+  );
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {title, url, techs} = request.body
+
+  const repositorie = {id:uuid(), title, url, techs, likes:0}
+
+  repositories.push(repositorie)
+
+  return response.status(201).send(repositorie)
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params
+  const {title, url, techs} = request.body
+
+  const repositorieindex = repositories.findIndex(repositorie=>repositorie.id === id)
+
+  if(repositorieindex < 0){
+    return response.status(400).json({error:`The project with id ${id} does not exists`})
+  }
+  const repositorie = {
+    id,
+    title,
+    url,
+    techs,
+    likes:0
+  }
+  repositories[repositorieindex] = repositorie
+
+  return response.status(200).send(repositorie)
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params
+
+  const repositorieindex = repositories.findIndex(repositorie=>repositorie.id === id)
+  if(repositorieindex < 0){
+    return response.status(400).json({error:`The project with id ${id} does not exists`})
+  }
+  repositories.splice(repositorieindex, 1)
+
+  return response.status(204).send()
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params
+
+  const repositorieIndex = repositories.findIndex(repositorie=>repositorie.id === id)
+
+  if(repositorieIndex < 0){
+    return response.status(400).json({error:`The project with id ${id} does not exists`})
+  }
+  const {title, url, techs, likes} = repositories[repositorieIndex]
+
+  const repositorie ={
+    id:repositories[repositorieIndex].id,
+    title:title,
+    url: url,
+    techs: techs,
+    likes: likes +1
+  } 
+
+  repositories[repositorieIndex] = repositorie;
+
+  return response.status(200).send(repositorie)
 });
 
 module.exports = app;
